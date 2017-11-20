4ed5d5e11dc997f5c937428e19787d667b004834
kelichau <tchau2@outlook.com>
2017-11-20 13:52:49 -0500

    Added array for alphabet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
-//Create array of alphabet
+//Create an array of the alphabet
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //Create an array of words for users to guess
 //Make it so that it'll pick a random word
+//Use onkeyup so that users can press any key to start the game
 //Create an answer array
 //Remaining letters 
 //Letters users have already guessed
