017df661c1d162ceb6928946c6acf1a578804e1d
kelichau <tchau2@outlook.com>
2017-11-20 13:10:01 -0500

    JavaScript code updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,12 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"
-                       "s", "t", "u", "v", "w", "x", "y", "z"];
-
-for(var i = 0; i < alphabet.length; i++){
-  //if the animal starts with c or o
-  if(alphabet[i].charAt(0) === "c" || alphabet[i].charAt(0) === "o"){
-  console.log(alphabet[i]);
-  }
-}
+                       "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                       "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var wins = 0;
 var guessRemaining = 12;
+var words = ["Creeper", "Steve", "Alex", "Biome", "Enderman", "Nether", "Creative", "Surival", "Mob", "Mojang", "Redstone", "Torch"
+              "Server", "Birchtree", "Jungle", "Desert"];
+var hints = ["Green and explodes", "Human version of zombie", "Girl in green shirt", "Desert, jungle, forest, snow", "Don't look in his eyes"]
 
 document.onkeyup = function(event)
 {
@@ -17,11 +14,16 @@ document.onkeyup = function(event)
   var userGuess = event.key;
 };
 
-for( var i = 0; i< word.length; i++)
-  {
-    console.log ("The loop ran")
-    $(."word-one").append("<span>___</span>");
-  };
+var word = words[Math.floor(Math.random() * words.length)];
+
+    var answerArray = [];
+    for (var i = 0; i < word.length; i++) {
+      answerArray[i] = "_";
+    }
+
+    var remainingLetters = word.length;
+        while (remainingLetters > 0) {
+
 
      //Use Key Events for letters the Player will type
     // Use onkeyup event listening
