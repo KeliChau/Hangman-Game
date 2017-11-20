00e83d40d337653fa3c8fdf031ec5fa42db5114b
kelichau <tchau2@outlook.com>
2017-11-20 13:50:51 -0500

    Pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,8 @@
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"
-                       "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-                       "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var wins = 0;
-var guessRemaining = 12;
-var words = ["Creeper", "Steve", "Alex", "Biome", "Enderman", "Nether", "Creative", "Surival", "Mob", "Mojang", "Redstone", "Torch"
-              "Server", "Birchtree", "Jungle", "Desert"];
-var hints = ["Green and explodes", "Human version of zombie", "Girl in green shirt", "Desert, jungle, forest, snow", "Don't look in his eyes"]
-
-document.onkeyup = function(event)
-{
-  console.log(event.key);
-  var userGuess = event.key;
-};
-
-var word = words[Math.floor(Math.random() * words.length)];
-
-    var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-      answerArray[i] = "_";
-    }
-
-    var remainingLetters = word.length;
-        while (remainingLetters > 0) {
+//Create array of alphabet
+//Create an array of words for users to guess
+//Make it so that it'll pick a random word
+//Create an answer array
+//Remaining letters 
+//Letters users have already guessed
+//Update with answer 
+//Alert user that they passed or to guess again
