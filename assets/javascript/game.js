c2ad31a29c8b12897aaed63fba1be5c1a11d3e55
Keli Chau <inquiries@kelichau.com>
2017-11-20 18:13:36 -0500

    Added JS codes to test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,50 @@
-//Array of the alphabet
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-//Array of words for the game
-var words = ["creeper", "enderman", "biome"];
-
-//Array for randomizing the words for players to guess
-var word = words[Math.floor(Math.random()*words.length];
-
-//Array for player's guesses
-var playerGuess = [];
-
-//Loop
-for (var i = 0; i < word.length; i++){
-  playerGuess[i] = "_";
-}
-
-//Remaining letters that the player hasn't used yet
-var remainingAlphabet = word.length;
-
-
-
-
-//Add a case string converter by using the toLowerCase method so players can type in capital or lowercase letters
+      var words = ['creeper', 'enderman', 'biome', 'steve', 'alex', 'diamond', 'ore', 'bunny', 'villager'];
+      var game = {
+        guessed: [],
+        left: 12,
+        start: function() {
+          this.complete = false;
+          this.word = words[Math.floor(Math.random() * words.length)];
+          this.$right = document.getElementById('right');
+          this.$wrong = document.getElementById('wrong');
+          this.$remain = document.getElementById('remain');
+          this.$right.innerHTML = '_'.repeat(this.word.length);
+        },
+        guess: function(letter) {
+          if (this.left > 0 && this.complete != true) {
+            if (this.word.indexOf(letter) > -1 || this.guessed.indexOf(letter) > -1) {
+              this.right(letter);
+            } else {
+              this.wrong(letter);
+            }
+          }
+        },
+        right: function(letter) {
+          for(var i = 0; i < this.word.length; i++) {
+            if (this.word[i] == letter) {
+              var word = this.$right.innerHTML.split('');
+              word[i] = letter;
+              this.$right.innerHTML = word.join('');
+            }
+          }
+          if (this.$right.innerHTML.indexOf('_') < 0) {
+            alert('Good job! You get yourself a piece of cake!');
+            this.complete = true;
+          }
+        },
+        wrong: function(letter) {
+          this.guessed.push(letter);
+          this.$wrong.innerHTML += ' ' + letter;
+          this.left--;
+          this.$remain.innerHTML = this.left;
+          if (this.left < 1) {
+            alert('You got blown up by a creeper! ' + 'The answer was:'+ this.word);
+            this.complete = true;
+          }
+        }
+      };
+      game.start();
+      document.onkeyup = function(event) {
+        var letter = String.fromCharCode(event.keyCode).toLowerCase();
+        game.guess(letter);
+      };
