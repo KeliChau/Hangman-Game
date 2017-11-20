a8199aeb4c7c9bb33ecaf0dd4d0e4ae58ef9f401
kelichau <tchau2@outlook.com>
2017-11-20 14:00:56 -0500

    Added variables for used alphabet and words for players to guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,16 @@
 //Create an array of the alphabet
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//Create an array of words for users to guess
+//Create an array for alphabet the player already used
+var usedAlphabet = [];
+//Create an array of words for players to guess
+var words = ["creeper", "enderman", "diamond sword"];
 //Make it so that it'll pick a random word
-//Use onkeyup so that users can press any key to start the game
+//Use onkeyup so that players can press any key to start the game
+document.onekeyup = function(event){
+
+}
 //Create an answer array
 //Remaining letters 
-//Letters users have already guessed
+//Letters players have already guessed
 //Update with answer 
-//Alert user that they passed or to guess again
+//Alert player that they passed or to play again
