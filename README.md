f2d920f76699007b99a6c28f85c8dfde74a5d1a1
kelichau <tchau2@outlook.com>
2017-12-19 18:13:34 -0500

    Adds working files

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # Hangman-Game
-## Hangman Game 
-A Minecraft-theme hangman game using jQuery
-### Current Status
-Adding hints to each word in the array
-## Future Plans
-+ Disable keyup function for keys that aren't letters (i.e. Enter, Control, Tab, numbers, etc.)
-+ Incorporate a Minecraft zombie or skeleton picture that will show each body part whenever a player misses a letter
-## Acknowledgements
-+ Tom McCarthy for his assistance in troubleshooting the js file
+Hangman Game (Challenge) Homework Week 2
+Using JavaScript, jQuery, HTML, and CSS to build a simple hangman game with a theme.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,89 @@
+//Beginning of document ready function
 $(document).ready(function(){
-	//Make it so that it'll pick a random word
-	//Create an array of the alphabet
-	var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	console.log(alphabet.length);
 
-	//Create an array for alphabet the player already used
+	//Empty array for letters used
 	var usedAlphabet = [];
-	console.log(usedAlphabet.length);
 
-	//Array of words for the game
-	var words = ["creeper", "enderman", "biome", "diamond", "zombie pigman"];
-	console.log(words.length);
-	//Loop
-	for (var i = 0; i < words.length; i++){
-	  console.log(words[i]);
-	  playerGuess[i] = "_";
+	//Array with words for player to guess
+	var words = ["creeper", "enderman", "biome", "diamond", "zombie pigman", "alex", "steve", "tnt", "skeleton", "grass block", 
+				"ender dragon", "iron sword", "coal", "ore", "clay", "ghast", "zombie", "gold", "bow", "boat", "glass", "birch tree", 
+				"crafting table", "potion", "pig", "cow", "realm", "mob", "nether", "mojang", "redstone", "sandbox", "server", "skin", 
+				"block", "brewing", "cheats", "hack", "dungeon", "farming", "furnace", "hopper", "lava", "minecon", "mining", 
+				"netherrack", "obsidian", "spawn", "spawn egg", "tools", "texture packs", "temple", "wither" ];
+	
+	//Array for resetGame
+	var word;
+
+	//Array to add underscore at beginning of guess
+	var underscore = "";
+
+	//Number guesses for player
+	var playerGuess = 12;
+
+	//Function to reset game/word whenever player loses
+	function resetGame(){
+		word = words[Math.floor(Math.random() * words.length)];	
+		underscore = "";	
+		for (var i = 0; i < word.length; i++){
+			underscore += "_ "
+		} 
+		playerGuess = 12;
+		$("#remain").text(playerGuess);		
+		$("#right").text("");
+		$("#right").text(underscore);		
 	}
+	resetGame(); 
 
-	//Array for randomizing the words for players to guess
-	var word = words[Math.floor(Math.random() * words.length];
+	//Beginning of keyup function for keyboard click
+	$(document).keyup(function(events){
 
-	//Array for player's guesses
-	var playerGuess = [];
-	console.log(playerGuess.length);
+		//Array to split word spaces	
+		var wordArray = word.split(""); 
 
-	//Remaining letters that the player hasn't used yet
-	var remainingAlphabet = word.length;
+		//Empty array for indices
+		var indices = [];
+		
+		//Beginning of for loop for word length
+		for (var i = 0; i < wordArray.length; i++){
+			if (events.key === wordArray[i]){
+				indices.push(i)
+			}
+		}
 
-	//Hint section for players, connected to the according words in the array
+		if (indices.length > 0) {	
+			var stringArray = $("#right").text().split(" ");
+			for (var i = 0; i < indices.length; i++){
+				stringArray.splice(indices[i], 1, events.key);
+			}
+			var prettyString = stringArray.join(" ");
+			$("#right").text(prettyString);
+		} else {
+			$("#wrong").append(events.key);
+			playerGuess-- 
+			//If player guess equals to 0, reset game
+			if(playerGuess === 0){
+				resetGame();
+			} else{ //Else, number of guesses remaining will decrease
+				$("#remain").text(playerGuess);
+			}
 
-	//Add letters that the players have already guessed
+		}	
 
-	//Update player with answer
+	});
 
-	//Alert the player that they won or to play again 
+	// //Beginning of hint function
+	// hint.onclick = function(){
+	// 	//Array with word hints
+	// 	hints = ["Blows up when you least expect it", "Don't look into his eyes", "Community of plants and animals", 
+	// 			"One of the most valuable resources for tools and armours", "Leave them alone and they won't bother you",
+	// 			"The main female protagonist", "The main male protagonist", "Explodes after ignition", "They're made of bones", "You can plant flowers on this",
+	// 			"You fight this mob in The End", "This is one of the first sharp fighting tool you can craft", "You use this to make the furnace heat up",
+	// 			"Coal, diamond, emerald, nether quartz, lapis lazuli, and redstone are this", "You need this to make brick", "Mob that lives in the nether and shoots fireballs",
+	// 			"Looks like an undead Steve"];
 
-	//Add a case string converter by using the toLowerCase method so players can type in capital or lowercase letters
+	// 	var hintIndex = 
 
-	//Reset button for players to refresh the game
-};
+	// };
+
+
+}); //End of document ready function
